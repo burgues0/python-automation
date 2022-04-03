fb2410941a1b21a79ea26ee538b66c043b25201f
burgues0 <victordpp@outlook.com>
2022-04-03 16:05:35 -0300

    adding cli args

--- multi-clipboard/multi-clipboard.py
+++ multi-clipboard/multi-clipboard.py
@@ -1,6 +1,33 @@
-import sys
+import getopt, sys
 import clipboard
 import json
 
-cb_data = clipboard.paste()
-print(cb_data)
+arg_list = sys.argv[1:]
+options = "hc:pl"
+long_options = ["help", "copy =", "paste", "list"]
+
+try:
+    arguments, values = getopt.getopt(arg_list, options, long_options)
+
+    for cur_arg, cur_value in arguments:
+        if(cur_arg in ("-h", "--help")):
+            print("\n Syntax \n     python multi-clipboard.py [options ...] \n\n Options \n\n -c [arg] | --copy [arg] \n Copies to your clipboard (Ctrl + V) the value of the data that corresponds to the key [arg].")
+        elif(cur_arg in ("-c", "--copy")):
+            print()
+        elif(cur_arg in ("-p", "--paste")):
+            print()
+        elif(cur_arg in ("-l", "--list")):
+            print()
+
+except getopt.error as err:
+    print(str(err))
+
+
+#if len(sys.argv) == 2:
+    #arg = sys.argv[1]
+
+    #if(arg != "save" and arg != "load" and arg != "list"):
+    #    print("Command not found.")
+
+#else:
+    #print("Invalid arguments. ")
