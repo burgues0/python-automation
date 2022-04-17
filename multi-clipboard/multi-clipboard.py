4e83609133e8cc36c02186b0e6582f5895f8211f
burgues0 <victordpp@outlook.com>
2022-04-17 00:12:46 -0300

    save and load functions

--- multi-clipboard/multi-clipboard.py
+++ multi-clipboard/multi-clipboard.py
@@ -6,6 +6,17 @@ arg_list = sys.argv[1:]
 options = "hc:pl"
 long_options = ["help", "copy =", "paste", "list"]
 
+#function to store the current clipboard into an json file
+def save_items(file_path, data):
+    with open(file_path, "w") as store_data:
+        json.dump(data, store_data)
+
+#returns the current saved items fron the json
+def load_items(file_path):
+    with open(file_path, "r") as load_data:
+        data = json.load(load_data)
+        return data
+
 try:
     arguments, values = getopt.getopt(arg_list, options, long_options)
 
@@ -21,13 +32,3 @@ try:
 
 except getopt.error as err:
     print(str(err))
-
-
-#if len(sys.argv) == 2:
-    #arg = sys.argv[1]
-
-    #if(arg != "save" and arg != "load" and arg != "list"):
-    #    print("Command not found.")
-
-#else:
-    #print("Invalid arguments. ")
