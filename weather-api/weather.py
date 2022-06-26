8f3cbf055383b5cdfbe2bd9a6afcb6a7d1d7d15e
burgues0 <victordpp@outlook.com>
2022-06-26 16:56:46 -0300

    add weather project start

---
+++ weather-api/weather.py
@@ -0,0 +1,7 @@
+import requests
+
+API_KEY = "f3b6901576ea0d42dd0b9ef604d5723f"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
