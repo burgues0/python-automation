489791216eb7a1887a813d57eb7526725c32b258
burgues0 <victordpp@outlook.com>
2022-06-26 17:03:50 -0300

    add weather project start

--- weather-api/weather.py
+++ weather-api/weather.py
@@ -5,3 +5,10 @@ BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 city = input("Enter a city name: ")
 request_url = f"{BASE_URL}?appid={API_KEY}&q={city}&units=metric"
+response = requests.get(request_url)
+
+if(response.status_code == 200):
+    data = response.json()
+    print(data)
+else:
+    print("Error. No data retrieved.")
